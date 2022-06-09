8c9e2815a44ea8ab81d53a97112a61c71922d33e
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-06-09 11:50:51 -0400

    update js, html, css, README

--- src/index.js
+++ src/index.js
@@ -1,15 +1,87 @@
+/* eslint-disable linebreak-style */
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+const todolist = [{
+  id: '',
+  todo: 'mmmmmmmmmmmm',
+  todostatus: '',
+}];
 
-  
-    return element;
+const submitbtn = document.getElementById('submit');
+submitbtn.addEventListener('click', () => {
+  const todoinput = document.getElementById('todo-input');
+  const inputtodo = todoinput.value;
+  addtodo(inputtodo);
+  todoinput.value = '';
+  showtodolist(todolist);
+});
+
+const addtodo = (inputtodo) => {
+  if (inputtodo.length < 1) {
+    const message = document.getElementById('message');
+    message.innerHTML = 'Please, type a "to do" activity';
+  } else {
+    const message = document.getElementById('message');
+    message.innerHTML = '';
+    todolist.push(
+      {
+        id: todolist.length > 0 ? todolist[todolist.length - 1].id + 1 : 1,
+        todo: inputtodo,
+        todostatus: true,
+      },
+    );
+    localStorage.setItem('todolist', JSON.stringify(todolist));
   }
-  
-  document.body.appendChild(component());
+};
+
+const showtodolist = (todolist) => {
+  const totallist = document.getElementById('totallist');
+  totallist.innerHTML = '';
+  for (let i = 1; i < todolist.length; i += 1) {
+    const line = document.createElement('div');
+    line.setAttribute('class', 'linelist');
+    const leftside = document.createElement('div');
+    leftside.setAttribute('id', 'leftside');
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    const activity = document.createElement('p');
+    activity.innerHTML = `${todolist[i].todo}`;
+    const divicon3 = document.createElement('div');
+    divicon3.setAttribute('class', 'change');
+    const icon3 = document.createElement('i');
+    icon3.setAttribute('class', 'fa-solid fa-ellipsis-vertical');
+
+    divicon3.addEventListener('click', () => {
+      icon3.setAttribute('class', 'fa-solid fa-trash-can');
+    });
+    divicon3.appendChild(icon3);
+    leftside.appendChild(checkbox);
+    leftside.appendChild(activity);
+    line.appendChild(leftside);
+    line.appendChild(divicon3);
+    totallist.appendChild(line);
+  }
+};
+
+const d = new Date();
+document.getElementById('today').innerHTML = d;
+
+/*
+if (localStorage.getItem('todolist') !== null && localStorage.getItem('todolist') !== undefined) {
+  Book.setBooks(JSON.parse(localStorage.getItem('todolist')));
+}
+*/
+/*
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Helloooooooooooooooooo', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
+*/
